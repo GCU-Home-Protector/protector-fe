370473afc7760667332d8ce3dc85160212214e4f
Lee Yubin <lyb110804@gmail.com>
2025-05-18 00:07:06 +0900

    백엔드에 전송 및 youtube player 패키지 추가

--- lib/camera_page.dart
+++ lib/camera_page.dart
@@ -1,7 +1,10 @@
+//import 'dart:ffi';
+import 'dart:ui' as ui;
+
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:path/path.dart' as path;
+// import 'package:path_provider/path_provider.dart';
+// import 'package:path/path.dart' as path;
 import 'package:http/http.dart' as http;
 
 import 'dart:async';
@@ -27,7 +30,7 @@ class _CamState extends State<CamPage>{
   // camera controller
   late CameraController _camController; // 카메라 초기화에서 사용
   late Future<void> _initializeControllerFuture; // camera 초기화
-  File? _ImgCapture;
+  // File? _ImgCapture;
 
   @override
   void initState(){
@@ -51,85 +54,128 @@ class _CamState extends State<CamPage>{
   Future<void> _CapturePicture() async {
     try{
       await _initializeControllerFuture;
-      final XFile? image = await _camController.takePicture();
+      final XFile image = await _camController.takePicture().timeout(Duration(seconds: 5));
+      
+      final File imageFile = File(image.path);
+      // Base 64 인코딩 과정
+      final bytes = await imageFile.readAsBytes();
+      final base64Img = base64Encode(bytes);
 
-      if (image != null){
-        final File imageFile = File(image.path);
-        // Base 64 인코딩 과정
-        final bytes = await imageFile.readAsBytes();
-        final base64Img = base64Encode(bytes);
+      // 백엔드에 이미지 전송 함수 호출 및 반환 받기
+      final response = await _sendImageToBackend(base64Img);
+      // await _sendImageToBackend(base64Img);
 
-        // 백엔드에 이미지 전송 함수 호출
-        // await _sendImageToBackend(base64Img);
-
-        if (mounted){
-          Navigator.push(
-            // base64 인코딩이 제대로 되었는지 확인하기 위해 RecomMusicPage에서 결과를 확인
-            context,
-            MaterialPageRoute(builder: (context) => RecomMusicPage(base64String: base64Img))
-            // context, MaterialPageRoute(builder: (context) => RecomMusicPage()),
-          );
-        }
+      if (mounted){
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            // 백엔드에서 받아올 반환 값을 음악 추천 페이지로 보내기
+            builder: (context) => RecomMusicPage(
+              songId: response['songId'] as int,
+              recommendSong: response['recommendSong'] as String,
+              recommendSongUrl: response['recommendSongUrl'] as String,
+            ),
+          ),
+          // context, MaterialPageRoute(builder: (context) => RecomMusicPage()),
+        );
       }
+      
+      // final XFile? image = await _camController.takePicture().timeout(Duration(seconds: 5));
+      // if (image == null){
+      //   return;
+      // }
+      // else{
+      //   final File imageFile = File(image.path);
+      //   // Base 64 인코딩 과정
+      //   final bytes = await imageFile.readAsBytes();
+      //   final base64Img = base64Encode(bytes);
+
+      //   // 백엔드에 이미지 전송 함수 호출 및 반환 받기
+      //   final response = await _sendImageToBackend(base64Img);
+      //   // await _sendImageToBackend(base64Img);
+
+      //   if (mounted){
+      //     Navigator.push(
+      //       context,
+      //       MaterialPageRoute(
+      //         // 백엔드에서 받아올 반환 값을 음악 추천 페이지로 보내기
+      //         builder: (context) => RecomMusicPage(
+      //           songId: response['songId'] as Long,
+      //           recommendSong: response['recommendSong'] as String,
+      //           recommendSongUrl: response['recommendSongUrl'] as String,
+      //           ),
+      //         ),
+      //       // context, MaterialPageRoute(builder: (context) => RecomMusicPage()),
+      //     );
+      //   }
+      // }
     } catch (e){
+      print('오류 발생 : $e');
       if (mounted){
         ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text("이미지 촬영에 실패하였습니다. ${e.toString()}"),));
+          SnackBar(content: Text("오류 발생 : ${e.toString()}"),));
       }
     }
   }
 
   // 백엔드 전송 함수
-  Future<void> _sendImageToBackend(String base64Img) async{
-    final url = Uri.parse('http://백엔드url.com/엔드포인트');  
-    http.MultipartRequest('POST', url);
-    
+  Future<Map<String, dynamic>> _sendImageToBackend(String base64Img) async{
+    const url = 'http://192.168.142.1:8080/music';  // 백엔드 서버 주소 -> ipconfig 해서 주소 변경 바람
+  
     try{
       final response = await http.post(
-        url,
+        Uri.parse(url),
         headers: {'Content-Type' : 'application/json'},
         body: jsonEncode({'image': base64Img}),
       );
 
-      if (response.statusCode != 200) {
-        throw Exception('서버 응답 없음 : ${response.statusCode}');
+      print('서버 응답 : ${response.body}'); // 백엔드 서버 응답답
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> data = jsonDecode(response.body);
+        return {
+          'songId': data['songId'] as int,
+          'recommendSong' : data['recommendSong'] as String,
+          'recommendSongUrl' : data ['recommendSongUrl'] as String,
+        };
+      } else {
+        throw Exception('서버 오류 : ${response.statusCode}');
       }
 
     } on http.ClientException catch (e){
-      throw Exception("${e.message}");
+      throw Exception("네트워크 오류 발생 : ${e.message}");
     }
   }
 
   // 사용자가 보는 화면 - 카메라, 촬영버튼
   @override
-Widget build(BuildContext context) {
-  return Scaffold(
-    body: FutureBuilder<void>(
-      future: _initializeControllerFuture,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          return Column(
-            children: [
-              Expanded(child: CameraPreview(_camController)),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 40, top: 16),
-                child: ElevatedButton(
-                  onPressed: _CapturePicture,
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: const Size(150, 40),
-                    padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Column(
+              children: [
+                Expanded(child: CameraPreview(_camController)),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 40, top: 16),
+                  child: ElevatedButton(
+                    onPressed: _CapturePicture,
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const ui.Size(150, 40),
+                      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                    ),
+                    child: const Text('촬영하기'),
                   ),
-                  child: const Text('촬영하기'),
                 ),
-              ),
-            ],
-          );
-        } else {
-          return const Center(child: CircularProgressIndicator());
-        }
-      },
-    ),
-  );
-}
+              ],
+            );
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+    );
+  }
 
 }
--- lib/recommend_music_page.dart
+++ lib/recommend_music_page.dart
@@ -1,40 +1,74 @@
+// import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+// import 'package:image/image.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class RecomMusicPage extends StatefulWidget{
-  final String base64String;
-  const RecomMusicPage({Key? key, required this.base64String}) : super (key: key);
+  // final String base64String;
+  // const RecomMusicPage({Key? key, required this.base64String}) : super (key: key);
+
+  final int songId;
+  final String recommendSong;
+  final String recommendSongUrl;
+
+  const RecomMusicPage({
+    super.key,
+    required this.songId,
+    required this.recommendSong,
+    required this.recommendSongUrl,
+  });
 
   @override
   State<RecomMusicPage> createState() => _RecomMusicState();  
 }
 
 class _RecomMusicState extends State<RecomMusicPage>{
+  // youtube player 선언
+  late YoutubePlayerController _youtubeController;
+  
   @override
-
-  // 인코딩된 이미지 확인하기 위한 코드
   void initState() {
-    // TERMINAL에 base64 문자열 출력하기
-    print("base64 String : ${widget.base64String}");
+    super.initState();
+    final videoId = YoutubePlayer.convertUrlToId(widget.recommendSongUrl) ?? '';
+    _youtubeController = YoutubePlayerController(
+      initialVideoId: videoId,
+      flags: const YoutubePlayerFlags(
+        autoPlay: true, // 자동 재생
+        mute: true,
+      ), 
+    );
   }
-
 
   @override
   Widget build (BuildContext context){
     // base64 인코딩된 string이 길다면 앞부분만 간략히 표시
-    final displayString = widget.base64String.length > 200
-    ? widget.base64String.substring(0, 200) + '...'
-    : widget.base64String;
+    // final displayString = widget.base64String.length > 200
+    // / ? widget.base64String.substring(0, 200) + '...'
+    // : widget.base64String;
 
     return Scaffold(
-      appBar: AppBar(title: const Text("Base64 인코딩 결과")),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(20),
-        child: SelectableText(
-          displayString,
-          style: const TextStyle(fontSize: 15),
-        ),
-        ),
+      appBar: AppBar(title: Text(widget.recommendSong)),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            YoutubePlayer(
+              controller: _youtubeController,
+              showVideoProgressIndicator: true,
+            ),
+            ElevatedButton(onPressed: () => _youtubeController.unMute(), 
+            child: const Text('음소거 해제'),
+            ),
+          ],),
+        )
       // 실제 : 백엔드에서 반환하는 음악 화면
-    );    
+    ); 
+  }
+
+  @override
+  void dispose() {
+    _youtubeController.dispose();
+    super.dispose();
   }
 }
